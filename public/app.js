ff5041620d651038d69e6e62cf9f72763fbdfa99
Abongile William <abongilewilliam@gmail.com>
2021-01-11 07:16:06 +0200

    complete for testing

--- public/app.js
+++ public/app.js
@@ -13,8 +13,8 @@ window.onscroll = function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function goToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = -10;
+  document.documentElement.scrollTop = -10;
 }
 
 
